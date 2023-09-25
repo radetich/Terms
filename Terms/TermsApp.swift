75fa6ac388582199c369d87c6d40ab9b856e827b
Cole Radetich <3dradd@gmail.com>
2023-09-24 18:25:45 -0600

    initial commit

---
+++ Terms/TermsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TermsApp.swift
+//  Terms
+//
+//  Created by Cole J Radetich on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct TermsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
